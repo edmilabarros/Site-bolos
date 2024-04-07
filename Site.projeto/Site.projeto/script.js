be7c5cad2cbe102115691ff18d6f1db3da87b959
edmilabarros <162130691+edmilabarros@users.noreply.github.com>
2024-04-07 17:59:51 +0100

    Add files via upload

---
+++ Site.projeto/Site.projeto/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona a classe "visible" ao menu após um pequeno atraso
+    setTimeout(function() {
+        document.querySelector(".menu").classList.add("visible");
+    }, 500); // Atraso de 500ms (0.5 segundos)
+
+
+    
+});
+
